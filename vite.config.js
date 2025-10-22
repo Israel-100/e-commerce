bb502383898f5f54a2633c8908964ed30c113331
Israel-100 <141845544+Israel-100@users.noreply.github.com>
2025-10-22 17:03:28 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const repo = process.env.GITHUB_REPOSITORY?.split('/')[1]
 // https://vite.dev/config/
 export default defineConfig({
+  base: repo ? `/${repo}/` : '/',
   plugins: [react()],
 })
+
+
